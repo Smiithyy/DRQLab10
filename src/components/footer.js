59b2287e003050a00189107e6fdd9303afb0ff30
Smiithyy <Smiithyy@GitHub.com>
2020-10-18 11:42:55 +0100

    First Commit

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'
+
+export class Footer extends React.Component {
+
+    render() {
+        return (
+            <div classname="App">
+                <h1>My Footer in another component</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'
+
+export class Header extends React.Component {
+
+    render() {
+        return (
+            <div classname="App">
+                <h1>My Header in another component</h1>
+            </div>
+        )
+    }
+}

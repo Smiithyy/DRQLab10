570493c6846eb8288dedf745c681f739e81d4c81
Smiithyy <Smiithyy@GitHub.com>
2020-11-19 17:44:02 +0000

    lab Complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 4000
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 app.use(cors())
 
@@ -20,28 +21,54 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+const myConnectionString = 'mongodb+srv://admin:Password1@cluster0.dodhn.mongodb.net/movies?retryWrites=true&w=majority';
+mongoose.connect(myConnectionString, {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    title:String,
+    year:String,
+    poster:String 
+});
+
+var MovieModel = mongoose.model("movie", movieSchema);
+
 app.get('/api/movies', (req, res) => {
 
-    const mymovies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        }];
+    // const mymovies = [
+    //     {
+    //         "Title": "Avengers: Infinity War",
+    //         "Year": "2018",
+    //         "imdbID": "tt4154756",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "Captain America: Civil War",
+    //         "Year": "2016",
+    //         "imdbID": "tt3498820",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    //     }
+    //];
 
-    res.status(200).json({
-        message: "Everything is ok",
-        movies: mymovies
-    });
+    MovieModel.find((err, data) => {
+        res.json(data);
+    })
+
+    // res.status(200).json({
+    //     message: "Everything is ok",
+    //     movies: mymovies
+    // });
+})
+
+app.get('/api/movies/:id', (req,res) => {
+    console.log(req.params.id);
+
+    MovieModel.findById(req.params.id, (err,data) => {
+        res.json(data);
+    })
 })
 
 app.post('/api/movies', (req, res) => {
@@ -49,6 +76,14 @@ app.post('/api/movies', (req, res) => {
     console.log(req.body.title)
     console.log(req.body.year)
     console.log(req.body.poster)
+
+    MovieModel.create({
+        title: req.body.title,
+        year: req.body.year,
+        poster: req.body.poster 
+    })
+
+    res.send('Item Added');
 })
 
 app.listen(port, () => {
